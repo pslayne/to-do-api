7f92be4606ab7bc43968e54abd53e17045fb5ce2
Layne Silveira <laynepsilveira@gmail.com>
2024-06-22 21:20:56 -0300

    refactor: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from './datasource/typeorm.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule, UsersModule, TasksModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
